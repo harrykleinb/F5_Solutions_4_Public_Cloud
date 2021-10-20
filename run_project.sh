638785d2c6f45b6bbd62a9b929e60de67100052b
Harrykleinb <haribok@yahoo.com>
2021-10-20 19:18:04 +0200

    ok

--- run_project.sh
+++ run_project.sh
@@ -2,5 +2,5 @@
 
 ansible-galaxy install -r ./Ansible_Playbooks/requirements.yml --force-with-deps
 
-ansible-playbook -e profile_aws="harry" -e region="eu-west-3" -e owner="harryk" ./Ansible_Playbooks/Create_Infra_AWS.yml --tags create
+ansible-playbook -e profile_aws="643019619955_Users" -e region="eu-west-3" -e owner="harryk" ./Ansible_Playbooks/Create_Infra_AWS.yml --tags create
 
