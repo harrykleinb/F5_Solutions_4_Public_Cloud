007df69f3f929f911fdb4c473723e511535b6bf3
Harrykleinb <haribok@yahoo.com>
2021-10-21 10:55:05 +0200

    ok

--- run_project.sh
+++ run_project.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook -e profile_aws="643019619955_Users" -e region="eu-west-3" -e owner="harryk" ./Ansible_Playbooks/Infra_AWS.yml --tags create
+ansible-playbook -vvv -e profile_aws="643019619955_Users" -e region="eu-west-3" -e owner="harryk" ./Ansible_Playbooks/Infra_AWS.yml --tags create
 
