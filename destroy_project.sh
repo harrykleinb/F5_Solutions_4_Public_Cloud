2846be515278436b962985f0127d53c28902e307
Harrykleinb <haribok@yahoo.com>
2021-11-09 18:57:30 +0100

    ok

--- destroy_project.sh
+++ destroy_project.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-playbook -e profile_aws="643019619955_Users" -e region="eu-west-2" -e owner="harryk" ./Ansible_Playbooks/Infra_AWS.yml --tags destroy
+ansible-playbook -e profile_aws="643019619955_Users" -e region="eu-west-2" -e owner="kleinbourg" ./Ansible_Playbooks/Infra_AWS.yml --tags destroy
 
