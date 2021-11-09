ac43d6c50bdc8b5671ce6c26062fe3e28b09c14a
Harrykleinb <haribok@yahoo.com>
2021-11-09 11:57:04 +0100

    ok

--- Ansible_Playbooks/K8S/Delete_Configuration_k8s_Cluster.sh
+++ Ansible_Playbooks/K8S/Delete_Configuration_k8s_Cluster.sh
@@ -3,14 +3,19 @@
 #see https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-manifests/
 
 #Delete the nginx-ingress namespace to uninstall the Ingress controller along with all the auxiliary resources that were created:
-$ kubectl delete namespace nginx-ingress
+kubectl delete namespace nginx-ingress
+
+#Delete namespace arcadia
+kubectl delete namespace arcadia
 
 #Delete the ClusterRole and ClusterRoleBinding:
-$ kubectl delete clusterrole nginx-ingress
-$ kubectl delete clusterrolebinding nginx-ingress
+kubectl delete clusterrole nginx-ingress
+kubectl delete clusterrolebinding nginx-ingress
 
 #Delete the Custom Resource Definitions:
 #Note: This step will also remove all associated Custom Resources.
-$ kubectl delete -f common/crds/
+kubectl delete -f ./K8S/common/crds/
+
+
 
 
